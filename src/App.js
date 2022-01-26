4905ad0eae1e88d0dac65d0dabf7c3b9b812c57c
Juan Gutierrez <gutierrezjuan.412@gmail.com>
2022-01-26 00:52:09 -0300

    Se agrego img de carrito. y contenedor de items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar'
-import { Button } from 'react-bootstrap';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
+  let items = {
+    title:"Anillo de Doran", 
+    text:"Da mana y poder de habilidad", 
+    src:'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltfeb794f72ab86ce5/5fa1ef94fe49b57a83a1717c/1056_Mage_T1_DoransRing.png'
+  }
   return (
     <>
     <NavBar 
       list={[{title: 'Inicio'}, {title: 'Categorias'}, {title: 'Vender'}, {title: 'Ofertas'}]}
       brand="Runeterra Store"
     />
-    <div className="App">
-      <label>Holi</label>
-    </div>
+    <ItemListContainer items={items}/>
     </>
   );
 }
---
+++ src/components/CartWidget.js
@@ -0,0 +1,7 @@
+const CartWidget = () =>{
+  return (
+    <img src='https://i.pinimg.com/originals/15/4f/df/154fdf2f2759676a96e9aed653082276.png' alt="Cart" width='60px' height='60px'/>
+  )
+}
+
+export default CartWidget
